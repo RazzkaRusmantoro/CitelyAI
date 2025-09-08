4781865849f2201965a9928603a13b4769dcac69
Razzka Rusmantoro <razzkarusmantoro123@gmail.com>
2025-09-08 13:29:58 +0400

    Changes

--- next-sitemap.config.ts
+++
@@ -1,9 +0,0 @@
-import { IConfig } from 'next-sitemap';
-
-const config: IConfig = {
-  siteUrl: "https://www.citera.net",
-  generateRobotsTxt: true,
-  sitemapSize: 7000,
-};
-
-export default config;
